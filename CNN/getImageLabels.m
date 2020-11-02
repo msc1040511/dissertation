dc294007f1ac1adfe2bc2af1ed4ef8a90e02b2a6
msc1040511 <73810901+msc1040511@users.noreply.github.com>
2020-11-02 00:17:13 -0500

    Update getImageLabels.m

--- CNN/getImageLabels.m
+++ CNN/getImageLabels.m
@@ -1,5 +1,5 @@
 function imageType = getImageLabels(imset)
-% Copyright (c) 2015, MathWorks, Inc.
+% Copyright (c) 2015, MathWorks, Inc
     imageType = categorical(repelem({imset.Description}', ...
         [imset.Count], 1));
 end
